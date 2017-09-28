0debe5783e1f8242c92ac4388bf131512d9eb6f5
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 21:53:58 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+  i = 0
+  while i < collection.length
+    yield collection[i]
+    new_collection.pop(yield collection[i])
+    i += 1
+  end
+  collection
 end
