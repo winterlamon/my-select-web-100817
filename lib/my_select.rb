4f6d8f9c1493ade7ad9afa136d204bc9088d3b97
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 21:58:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+  i = 0
+  while i < collection.length
+    if yield collection[i]
+    new_collection.push(collection[i])
+    end
+    i += 1
+  end
+  new_collection
 end
